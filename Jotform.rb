64fe313c58a19e543a86c89e4fcea3f03806e616
uanseru <uanseru@gmail.com>
2020-07-29 00:12:57 +0200

    Eureka
    starting to make a little progress

--- Jotform.rb
+++ Jotform.rb
@@ -1,4 +1,10 @@
 require 'watir'
 browser = Watir::Browser.new
-browser.goto("https://www.google.com")
+browser.goto("https://form.jotform.com/201882323530347")
+sleep 5.00
+# This clicks on the next button
+browser.button(text: 'Next').click
 
+
+
+sleep 5.00
