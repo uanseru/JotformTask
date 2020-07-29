16df2b9ddbc09af030daf8174249f9528c096441
uanseru <uanseru@gmail.com>
2020-07-29 13:16:47 +0200

    Signiture page
    Learning more and trying to complete a hapy path

--- Jotform.rb
+++ Jotform.rb
@@ -20,11 +20,16 @@ sleep 3.00
 browser.button(css: 'div#cid_3 > div > div:nth-of-type(3) > button:nth-of-type(2)').click
 sleep 3.00
 
-# Using a Css selector to click the next button
-browser.button(css: 'div#cid_3 > div > div:nth-of-type(3) > button:nth-of-type(2)').click
-sleep 3.00
 
-browser.span(css: 'div#cardProgress > div > div:nth-of-type(4) > div > div > span').click
+# Using a Xpath selector to Click on next button
+button = browser.div(class: /jfCard-actions/).button(xpath: '//li[2]//div[1]//div[1]//div[3]//button[2]').wait_until(timeout: 100, &:present?)
+button.fire_event :onclick
+
+
+
+#browser.span(css: 'div#cardProgress > div > div:nth-of-type(4) > div > div > span').click
 
 #browser.button(class: 'jfInput-button forNext u-right').click
 sleep 3.00
+
+
